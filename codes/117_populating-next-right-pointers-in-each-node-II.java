e08ee9026ddd92e3179bd51d9d8e851d0454f418
Kevin Wong <nyckevinwong@gmail.com>
2020-12-16 12:30:03 -0500

    updated

--- codes/117_populating-next-right-pointers-in-each-node-II.java
+++ codes/117_populating-next-right-pointers-in-each-node-II.java
@@ -1,22 +1,22 @@
 public static Node connect(Node root) {            
-    Node nextHead = new Node(0);
-    nextHead.next = root;
+    Node nextHead = new Node(0); //用dummy node
+    nextHead.next = root; 
     
-    while(nextHead.next != null)
+    while(nextHead.next != null) // 如果根或next不為空
     {
-        Node tail = nextHead;
-        Node n = nextHead.next;
+        Node prev = nextHead;
+        Node cur = nextHead.next;
         nextHead.next = null;
         
-        for(; n != null; n = n.next){
-            if(n.left != null){
-                tail.next = n.left;
-                tail = tail.next;
+        for(; cur != null; cur = cur.next){
+            if(cur.left != null){
+                prev.next = cur.left;
+                prev = prev.next;
             }
             
-            if(n.right != null){
-                tail.next = n.right; 
-                tail = tail.next;
+            if(cur.right != null){
+                prev.next = cur.right; 
+                prev = prev.next;
             }
         }
     }
